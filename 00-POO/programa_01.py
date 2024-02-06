44b36de4e5b01209b205b0dd6ed47cc48b4c4cf5
Didd12 <enzoflores197@gmail.com>
2024-02-06 10:33:03 -0600

    Se agrego la validacion de numeros enteros

--- 00-POO/programa_01.py
+++ 00-POO/programa_01.py
@@ -1,51 +1,97 @@
-
+carreras=[]
+clases=[]
 seguir = True
-carreras = []
+seguir_clases = True
 
+ 
 while seguir:
+    print(carreras)
+    print("--------------MENU--------------")
+    print("1.Crear Carrera")
+    print("2.Leer carrera")
+    print("3.Actualizar carrera")
+    print("4.Borrar carrera")
+    print("5.Salir")
+    print("6.Agregar clases")
+    print("--------------------------------")
+    option = input("Ingrese su opcion: ")
     
-    print("****************")
-    print("Menu")
-    print("1. Crear carrera")
-    print("2. Leer carrera")
-    print("3. Actualizar carrera")
-    print("4. Borrar Carrera")
-    print("5. Salir")
-    
-    print("****************")
-    
-    opcion = int (input("Ingrese su opcion:"))
-    
-    
-    
-    print("*")
-    if opcion==1:
-        print("Ingresar carrera")
-        nombre = input("Nombre:")
-        dicCarrera={}
-        dicCarrera["carrera"]= nombre
-        carreras.append(dicCarrera)
-        print("Carrera agregada exitosamente")
+    if option.isnumeric():
+        opcion = int(option)
+        if opcion == 1:
+            print("Ingresar carrera")
+            nombre = input("Nombre: ")
+            dicCarrea={}
+            dicCarrea["carrera"] = nombre
+            carreras.append(dicCarrea)
         
-    elif opcion==2:
-        print("Leer (Mostrar) carrera")
-        for carrera in carreras:
-            print("-Nombre:"+carrera["carrera"])
+        if opcion == 2:
+            print("Leyendo las carreras")
+            for carrera in carreras:
+                print(" Nombre : " + carrera["carrera"])
             
-    elif opcion ==3:
-        print("Actualizar carrera")
-        nombre = input("Ingrese el nombre de la carrera:")
+        if opcion == 3:
+            car_remplazar=input("carrera a sustituir: ")
+            new_carrera=input("Ingrese nueva carrea: ")
+            dicCarrea={}
+            dicCarrea["carrera"] = new_carrera
+            indice = 0
+            for carrera in carreras:
+                if carrera["carrera"] == car_remplazar:
+                    carreras[indice] = dicCarrea
+                    break
+                else :
+                    indice = indice + 1
         
-        
-    elif opcion==4:
-        print("Borrar carrera")
-        
-        
-    elif opcion==5:
-        print("Hasta la proxima")
-        
-        seguir==False
-print("")
+        if opcion == 4:
+            print("Borrar Carrera")
+            carrera_borrar = input("Ingrese el nombre de la carrera que desea borrar:")
+            indice = 0
+            for carrera in carreras:
+                if carrera["carrera"] == carrera_borrar:
+                    carreras.remove(carrera)
+                    break
+                else :
+                    indice = indice + 1
+        if opcion == 5:
+            print("Hasta la Proxima")
+            seguir = False
+        elif opcion == 6:
+            while seguir_clases:
+              print("--------------MENU--------------")
+              print("1.Crear clase")
+              print("2.Leer carrera")
+              print("3.Actualizar carrera")
+              print("4.Borrar carrera")
+              print("5.Salir")
+              print("6.Salir")
+              print("--------------------------------")
+              option_class = input("Ingrese su opcion")
+              
+              if option_class.isnumeric():
+                  opcion_class = int(option_class)
+                  
+                  if opcion_class == 1:
+                       print("Ingresar clase")
+                       nombre = input("Nombre Clase: ")
+                       dicClases={}
+                       dicClases["clases"] = nombre
+                       clases.append(dicClases)
+                       
+                  elif opcion_class ==6:
+                      print("Sale bye")
+                      seguir_clases = False
+              else:
+                  print("Entrada incorrecta, favor ingresar un numero del menu(1-5)")
+                  
+                  
+              
+                
+                  
+             
+    else :
+        print("Entrada incorrecta, favor ingresar un numero del menu(1-5)")
+    print("--------------------------------")
 
 """
     entrada = input("Ingresa un número entero: ")
@@ -53,4 +99,8 @@ if validar_entero(entrada):
     print("La entrada es un número entero.")
 else:
     print("La entrada no es un número entero.")
+    
+if opcion.isnumeric()
+
+else:
 """
